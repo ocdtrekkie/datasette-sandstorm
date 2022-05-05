2687649071e97d3064b5fa1d8235fdbcfd1a403b
Jacob Weisz <inbox@jacobweisz.com>
2022-05-05 10:45:09 -0500

    Populate version, add a couple plugins

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -22,5 +22,6 @@ set -euo pipefail
 cd /opt/app
 
 sudo pip3 install datasette
-sudo datasette install datasette-upload-csvs
+sudo datasette install -U datasette datasette-copyable datasette-total-page-time datasette-upload-csvs
+datasette --version | cut -d ' ' -f 3 > .sandstorm/version
 exit 0
