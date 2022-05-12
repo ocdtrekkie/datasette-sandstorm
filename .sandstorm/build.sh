b438ef3ed730968b2e1a7ba18602542796fe9ff2
Jacob Weisz <inbox@jacobweisz.com>
2022-05-12 01:28:55 -0500

    Add several more plugins

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 cd /opt/app
 
 sudo pip3 install datasette
-sudo datasette install -U datasette datasette-cluster-map datasette-copyable datasette-dateutil datasette-geojson-map datasette-graphql datasette-gzip datasette-haversine datasette-hovercards datasette-json-html datasette-jupyterlite datasette-leaflet-freedraw datasette-nteract-data-explorer datasette-packages datasette-pretty-traces datasette-pyinstrument datasette-render-markdown datasette-schema-versions datasette-template-request datasette-tiddlywiki datasette-total-page-time datasette-upload-csvs datasette-write sqlite-colorbrewer
+sudo datasette install -U datasette datasette-atom datasette-backup datasette-basemap datasette-cluster-map datasette-configure-fts datasette-copyable datasette-dateutil datasette-edit-schema datasette-geojson-map datasette-graphql datasette-gzip datasette-haversine datasette-hovercards datasette-ics datasette-jellyfish datasette-json-html datasette-jupyterlite datasette-leaflet-freedraw datasette-leaflet-geojson datasette-nteract-data-explorer datasette-packages datasette-placekey datasette-pretty-json datasette-pretty-traces datasette-pyinstrument datasette-render-images datasette-render-markdown datasette-schema-versions datasette-search-all datasette-template-request datasette-tiddlywiki datasette-tiles datasette-total-page-time datasette-upload-csvs datasette-write datasette-yaml sqlite-colorbrewer
 datasette --version | cut -d ' ' -f 3 > .sandstorm/version
 exit 0
