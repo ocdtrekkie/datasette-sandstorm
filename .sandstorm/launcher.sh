09009172172f8e64f4b9c73f4148637656f75a44
Jacob Weisz <inbox@jacobweisz.com>
2022-05-17 00:11:53 -0500

    Upload databases

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 cd /opt/app
-touch /var/data.sqlite
-datasette /var/data.sqlite --root
+#touch /var/data.sqlite
+#datasette /var/data.sqlite --root
+datasette -m metadata.yml --root
 
 exit 0
